037643f03f8742d9ffa6f72455aa5a14f754f84f
Siddharth shah <132256541+siddharth-coder8@users.noreply.github.com>
2023-05-04 09:48:29 +0530

    Shopping.php
    This is a website template of amazon

---
+++ shopping.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Amazon</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<header>
+		<nav>
+			<ul>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">Shop</a></li>
+				<li><a href="#">Deals</a></li>
+				<li><a href="#">Services</a></li>
+				<li><a href="#">Help</a></li>
+			</ul>
+		</nav>
+		<div class="search-bar">
+			<input type="text" placeholder="Search...">
+			<button>Search</button>
+		</div>
+		<div class="cart-icon">
+			<a href="#"><img src="cart-icon.png"></a>
+		</div>
+	</header>
+
+	<main>
+		<div class="category-menu">
+			<ul>
+				<li><a href="#">Electronics</a></li>
+				<li><a href="#">Clothing</a></li>
+				<li><a href="#">Books</a></li>
+				<li><a href="#">Home &amp; Kitchen</a></li>
+				<li><a href="#">Sports &amp; Outdoors</a></li>
+				<li><a href="#">Beauty &amp; Personal Care</a></li>
+			</ul>
+		</div>
+		<div class="product-list">
+			<div class="product">
+				<img src="product1.jpg">
+				<h3>Product 1</h3>
+				<p>$99.99</p>
+				<button>Add to Cart</button>
+			</div>
+			<div class="product">
+				<img src="product2.jpg">
+				<h3>Product 2</h3>
+				<p>$49.99</p>
+				<button>Add to Cart</button>
+			</div>
+			<div class="product">
+				<img src="product3.jpg">
+				<h3>Product 3</h3>
+				<p>$149.99</p>
+				<button>Add to Cart</button>
+			</div>
+			<div class="product">
+				<img src="product4.jpg">
+				<h3>Product 4</h3>
+				<p>$199.99</p>
+				<button>Add to Cart</button>
+			</div>
+		</div>
+	</main>
+
+	<footer>
+		<p>&copy; 2023 Amazon. All rights reserved.</p>
+	</footer>
+</body>
+</html>
